1c5418279e9e91ff4d741cf1bbdc4a6395b9624f
cabooster <71710781+cabooster@users.noreply.github.com>
2021-08-22 18:08:00 +0800

    Update script.py

--- script.py
+++ script.py
@@ -7,9 +7,9 @@ flag = sys.argv[1]
 # Only train, using 1 GPU and batch_size=1
 if flag == 'train':
     os.system('python train.py --datasets_folder DataForPytorch \
-                               --n_epochs 40 --GPU 0 --batch_size 1 \
+                               --n_epochs 30 --GPU 0 --batch_size 1 \
                                --img_h 150 --img_w 150 --img_s 150 \
-                               --train_datasets_size 3000')  
+                               --train_datasets_size 3500')  
 
 if flag == 'test':
     os.system('python test.py --denoise_model ModelForPytorch --datasets_folder DataForPytorch \
