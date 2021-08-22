9579f0f48302b2e039df30589f4fb8615f0333a7
cabooster <71710781+cabooster@users.noreply.github.com>
2021-08-22 11:27:25 +0800

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ if flag == 'train':
     os.system('python train.py --datasets_folder DataForPytorch \
                                --n_epochs 40 --GPU 0,1 --batch_size 2 \
                                --img_h 150 --img_w 150 --img_s 150 \
-                               --train_datasets_size 3500')  
+                               --train_datasets_size 3000')  
 
 if flag == 'test':
     os.system('python test.py --denoise_model ModelForPytorch --datasets_folder DataForPytorch \
