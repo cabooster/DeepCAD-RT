2df128a24749b1297c4e2358c6c7b88561b99901
Xinyang Li <71710781+cabooster@users.noreply.github.com>
2024-08-27 13:45:38 +0800

    Update data_process.py

--- DeepCAD_RT_pytorch/deepcad/data_process.py
+++ DeepCAD_RT_pytorch/deepcad/data_process.py
@@ -176,7 +176,7 @@ def train_preprocess_lessMemoryMulStacks(args):
     print('Total stack number -----> ', stack_num)
 
     print('Reading files...')   
-    print('\033[1;33mPlease check the shape of these image stacks, since some hyperstacks will have unusual shapes. In that case, you just need to re-store these images by ImageJ. \033[0m') 
+    print('\033[1;33mPlease check the shape of these image stacks, since some hyperstacks have unusual shapes. In that case, you just need to re-store these images by ImageJ. \033[0m') 
     for im_name in list(os.walk(im_folder, topdown=False))[-1][-1]:
         print(im_name)
         im_dir = im_folder + '//' + im_name
