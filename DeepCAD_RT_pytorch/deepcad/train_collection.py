19f039aa73a4744e954ede517c0c4dfb8f9a9b98
Xinyang Li <71710781+cabooster@users.noreply.github.com>
2025-04-27 16:53:02 +0800

    Update train_collection.py

--- DeepCAD_RT_pytorch/deepcad/train_collection.py
+++ DeepCAD_RT_pytorch/deepcad/train_collection.py
@@ -389,7 +389,7 @@ class training_class():
             # Pre-trained models are loaded into memory and the sub-stacks are directly fed into the model.
             noise_patch = noise_patch.cuda()
             real_A = noise_patch
-            real_A = Variable(real_A)
+            real_A = real_A.float()
             fake_B = self.local_model(real_A)
 
             # Determine approximate time left
